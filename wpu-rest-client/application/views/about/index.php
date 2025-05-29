8e7de6963a83b678d246688611c4c3f7c10f6258
dyntamela <ocolcingku@gmail.com>
2025-05-29 21:40:34 +0700

    Menyelesaikan Vidio 9

---
+++ wpu-rest-client/application/views/about/index.php
@@ -0,0 +1,75 @@
+<!-- About -->
+<div class="container">
+  <div class="row mb-4">
+    <div class="col text-center">
+      <h2>About My Project</h2>
+    </div>
+  </div>
+
+  <!-- Portfolio -->
+      <section class="portfolio" id="portfolio">
+          <div class="row">
+            <div class="col-md mb-4">
+              <div class="card">
+                <img class="card-img-top" src="<?= base_url(); ?>assets/img/thumbs/1.png" alt="Card image cap">
+                <div class="card-body">
+                  
+                  <p class="card-text">Aktif menulis artikel untuk berbagi ide dan wawasan secara konsisten di Medium.</p>
+                  
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md mb-4">
+              <div class="card">
+                <img class="card-img-top" src="<?= base_url(); ?>assets/img/thumbs/2.png" alt="Card image cap">
+                <div class="card-body">
+                  
+                  <p class="card-text">Membangun website pengelolaan gaji yang otomatis, aman, dan mudah digunakan.</p>
+                  
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md mb-4">
+              <div class="card">
+                <img class="card-img-top" src="<?= base_url(); ?>assets/img/thumbs/3.png" alt="Card image cap">
+                <div class="card-body">
+                  
+                  <p class="card-text">Membuat platform laporan kerja yang memudahkan pencatatan dan monitoring aktivitas.</p>
+                                 
+              </div>
+            </div>   
+          </div>
+
+          <div class="row">
+            <div class="col-md mb-4">
+              <div class="card">
+                <img class="card-img-top" src="<?= base_url(); ?>assets/img/thumbs/4.png" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text">Merancang antarmuka laporan karyawan yang sederhana, nyaman, dan efektif.</p>
+            
+                </div>
+              </div>
+            </div> 
+            <div class="col-md mb-4">
+              <div class="card">
+                <img class="card-img-top" src="<?= base_url(); ?>assets/img/thumbs/5.png" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text">Mengembangkan sistem arsip surat digital dengan pencarian cepat dan pengelolaan efisien.
+                  </p>
+                 
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md mb-4">
+              <div class="card">
+                <img class="card-img-top" src="<?= base_url(); ?>assets/img/thumbs/6.png" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text">Menyempurnakan tampilan dan navigasi Edlink agar lebih modern dan user-friendly.</p>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
